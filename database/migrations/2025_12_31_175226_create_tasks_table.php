ea78449afca7eacdd48999c797a02fd64584831b
Arivan Ismail <187494087+arivanismail-05@users.noreply.github.com>
2025-12-31 20:55:35 +0300

    Add migrations for categories and tasks tables with relationships

--- database/migrations/2025_12_31_175226_create_tasks_table.php
+++ database/migrations/2025_12_31_175226_create_tasks_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
+            $table->boolean('is_done')->default(false);
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('tasks');
     }
 };
